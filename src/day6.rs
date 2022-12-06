7fb01f60a3dad117bd7c2c8252dd0be9ee4b0088
Mattias Buelens <mattias@buelens.com>
2022-12-06 09:23:26 +0100

    Solve day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -3,12 +3,9 @@ pub fn input_generator(input: &str) -> String {
     input.to_string()
 }
 
-const MARKER_LEN: usize = 4;
+const PACKET_MARKER_LEN: usize = 4;
 
 fn is_marker(s: &[u8]) -> bool {
-    if s.len() != MARKER_LEN {
-        return false;
-    }
     for (i, b) in s.iter().enumerate() {
         if s[(i + 1)..].contains(b) {
             return false;
@@ -19,18 +16,26 @@ fn is_marker(s: &[u8]) -> bool {
 
 #[aoc(day6, part1)]
 pub fn part1(input: &str) -> usize {
-    let (pos, _marker) = input
+    let (pos, marker) = input
         .as_bytes()
-        .windows(MARKER_LEN)
+        .windows(PACKET_MARKER_LEN)
         .enumerate()
         .find(|(_, s)| is_marker(s))
         .unwrap();
-    pos + MARKER_LEN
+    pos + marker.len()
 }
 
+const MESSAGE_MARKER_LEN: usize = 14;
+
 #[aoc(day6, part2)]
-pub fn part2(input: &str) -> i32 {
-    todo!()
+pub fn part2(input: &str) -> usize {
+    let (pos, marker) = input
+        .as_bytes()
+        .windows(MESSAGE_MARKER_LEN)
+        .enumerate()
+        .find(|(_, s)| is_marker(s))
+        .unwrap();
+    pos + marker.len()
 }
 
 #[cfg(test)]
@@ -38,18 +43,28 @@ mod tests {
     use super::*;
 
     lazy_static! {
-        static ref TEST_INPUT: &'static str = r"mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        static ref INPUT1: &'static str = r"mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        static ref INPUT2: &'static str = r"bvwbjplbgvbhsrlpgdmjqwftvncz";
+        static ref INPUT3: &'static str = r"nppdvjthqldpwncqszvftbrmjlhg";
+        static ref INPUT4: &'static str = r"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        static ref INPUT5: &'static str = r"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
     }
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(&TEST_INPUT), 7);
-        assert_eq!(part1(&"bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(part1(&INPUT1), 7);
+        assert_eq!(part1(&INPUT2), 5);
+        assert_eq!(part1(&INPUT3), 6);
+        assert_eq!(part1(&INPUT4), 10);
+        assert_eq!(part1(&INPUT5), 11);
     }
 
     #[test]
     fn test_part2() {
-        let input = input_generator(&TEST_INPUT);
-        assert_eq!(part2(&input), 0);
+        assert_eq!(part2(&INPUT1), 19);
+        assert_eq!(part2(&INPUT2), 23);
+        assert_eq!(part2(&INPUT3), 23);
+        assert_eq!(part2(&INPUT4), 29);
+        assert_eq!(part2(&INPUT5), 26);
     }
 }
