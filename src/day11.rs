7f6bb839c42a83131213416eaf5110de04860ab1
Mattias Buelens <mattias@buelens.com>
2022-12-11 17:43:21 +0100

    Solve day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,12 +1,14 @@
 use std::cmp::Reverse;
 use std::mem;
 
+use crate::util::lcm_64;
+
 #[derive(Debug, Clone)]
 pub struct Monkey {
     number: usize,
-    items: Vec<i32>,
+    items: Vec<i64>,
     operation: Operation,
-    test: i32,
+    test: i64,
     throw_if_true: usize,
     throw_if_false: usize,
     total_inspected: usize,
@@ -14,8 +16,8 @@ pub struct Monkey {
 
 #[derive(Debug, Copy, Clone)]
 pub enum Operation {
-    Add(i32),
-    Multiply(i32),
+    Add(i64),
+    Multiply(i64),
     Square,
 }
 
@@ -37,7 +39,7 @@ pub fn input_generator(input: &str) -> Vec<Monkey> {
             .strip_prefix("  Starting items: ")
             .unwrap()
             .split(", ")
-            .map(|item| item.parse::<i32>().unwrap())
+            .map(|item| item.parse::<i64>().unwrap())
             .collect();
         let operation = lines
             .next()
@@ -83,9 +85,23 @@ pub fn input_generator(input: &str) -> Vec<Monkey> {
 
 struct Game {
     monkeys: Vec<Monkey>,
+    part2: bool,
+    test_lcm: i64,
 }
 
 impl Game {
+    fn new(monkeys: Vec<Monkey>, part2: bool) -> Self {
+        let mut test_lcm = 1;
+        for monkey in monkeys.iter() {
+            test_lcm = lcm_64(test_lcm, monkey.test);
+        }
+        Self {
+            monkeys,
+            part2,
+            test_lcm,
+        }
+    }
+
     fn round(&mut self) {
         for i in 0..self.monkeys.len() {
             self.turn(i);
@@ -99,6 +115,7 @@ impl Game {
         monkey.total_inspected += items.len();
         // Mutate other monkeys
         let &Monkey {
+            number,
             operation,
             test,
             throw_if_true,
@@ -107,11 +124,13 @@ impl Game {
         } = &*monkey;
         for mut item in items {
             item = match operation {
-                Operation::Add(value) => item + value,
-                Operation::Multiply(value) => item * value,
-                Operation::Square => item * item,
+                Operation::Add(value) => (item + value) % self.test_lcm,
+                Operation::Multiply(value) => (item * value) % self.test_lcm,
+                Operation::Square => (item * item) % self.test_lcm,
             };
-            item /= 3;
+            if !self.part2 {
+                item /= 3;
+            }
             if item % test == 0 {
                 self.monkeys[throw_if_true].items.push(item);
             } else {
@@ -123,9 +142,7 @@ impl Game {
 
 #[aoc(day11, part1)]
 pub fn part1(input: &[Monkey]) -> usize {
-    let mut game = Game {
-        monkeys: input.to_vec(),
-    };
+    let mut game = Game::new(input.to_vec(), false);
     for _ in 0..20 {
         game.round();
     }
@@ -135,8 +152,14 @@ pub fn part1(input: &[Monkey]) -> usize {
 }
 
 #[aoc(day11, part2)]
-pub fn part2(input: &[Monkey]) -> i32 {
-    todo!()
+pub fn part2(input: &[Monkey]) -> usize {
+    let mut game = Game::new(input.to_vec(), true);
+    for _ in 0..10000 {
+        game.round();
+    }
+    game.monkeys
+        .sort_unstable_by_key(|monkey| Reverse(monkey.total_inspected));
+    game.monkeys[0].total_inspected * game.monkeys[1].total_inspected
 }
 
 #[cfg(test)]
@@ -150,7 +173,7 @@ mod tests {
     #[test]
     fn test_part1_example() {
         let monkeys = input_generator(&TEST_INPUT);
-        let mut game = Game { monkeys };
+        let mut game = Game::new(monkeys, false);
         game.round();
         assert_eq!(game.monkeys[0].items, vec![20, 23, 27, 26]);
         assert_eq!(game.monkeys[1].items, vec![2080, 25, 167, 207, 401, 1046]);
@@ -174,6 +197,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let input = input_generator(&TEST_INPUT);
-        assert_eq!(part2(&input), 0);
+        assert_eq!(part2(&input), 2713310158);
     }
 }
