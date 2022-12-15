9a565d886f01eafe0d0e2554ca1c4921e8376a9f
Mattias Buelens <mattias@buelens.com>
2022-12-15 19:58:43 +0100

    Solve day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -27,12 +27,15 @@ pub fn input_generator(input: &str) -> Vec<SensorReading> {
         .collect()
 }
 
+fn is_within_reading_range(reading: &SensorReading, pos: Vector2D) -> bool {
+    (pos - reading.sensor).manhattan_distance()
+        <= (reading.beacon - reading.sensor).manhattan_distance()
+}
+
 fn is_position_without_beacon(readings: &[SensorReading], pos: Vector2D) -> bool {
-    readings.iter().any(|reading| {
-        pos != reading.beacon
-            && (pos - reading.sensor).manhattan_distance()
-                <= (reading.beacon - reading.sensor).manhattan_distance()
-    })
+    readings
+        .iter()
+        .any(|reading| pos != reading.beacon && is_within_reading_range(reading, pos))
 }
 
 fn count_positions_without_beacons(readings: &[SensorReading], y: i32) -> usize {
@@ -56,9 +59,41 @@ pub fn part1(input: &[SensorReading]) -> usize {
     count_positions_without_beacons(input, 2_000_000)
 }
 
+fn could_be_distress_beacon(readings: &[SensorReading], pos: Vector2D) -> bool {
+    readings
+        .iter()
+        .all(|reading| pos != reading.beacon && !is_within_reading_range(reading, pos))
+}
+
+fn find_distress_beacon(readings: &[SensorReading], max_coord: i32) -> Vector2D {
+    for y in 0..=max_coord {
+        let mut x = 0;
+        while x <= max_coord {
+            let pos = Vector2D::new(x, y);
+            if let Some(reading) = readings
+                .iter()
+                .find(|reading| is_within_reading_range(reading, pos))
+            {
+                // Skip ahead along this row until we're out of range of this reading.
+                let to_beacon = reading.beacon - reading.sensor;
+                let to_pos = pos - reading.sensor;
+                x = reading.sensor.x() + to_beacon.manhattan_distance() - to_pos.y().abs() + 1;
+                debug_assert!(is_within_reading_range(reading, Vector2D::new(x - 1, y)));
+                debug_assert!(!is_within_reading_range(reading, Vector2D::new(x, y)));
+            } else {
+                // No sensor reading in range, this must be the distress beacon!
+                return pos;
+            }
+        }
+    }
+    panic!("distress beacon not found")
+}
+
 #[aoc(day15, part2)]
-pub fn part2(input: &[SensorReading]) -> i32 {
-    todo!()
+pub fn part2(input: &[SensorReading]) -> i64 {
+    let max_coord = 4_000_000;
+    let pos = find_distress_beacon(input, max_coord);
+    (pos.x() as i64) * (max_coord as i64) + (pos.y() as i64)
 }
 
 #[cfg(test)]
@@ -78,6 +113,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let input = input_generator(&TEST_INPUT);
-        assert_eq!(part2(&input), 0);
+        assert_eq!(find_distress_beacon(&input, 20), Vector2D::new(14, 11));
     }
 }
