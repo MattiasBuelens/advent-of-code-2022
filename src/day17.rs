086d656dcc910f0a6d927ece4e905fe5e1f3e6fc
Mattias Buelens <mattias@buelens.com>
2022-12-18 20:55:57 +0100

    Solve day 17 part 2

--- src/day17.rs
+++ src/day17.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::util::Vector2D;
 
@@ -112,11 +112,11 @@ impl Tower {
         }
     }
 
-    fn drop_rock(&mut self, rock: RockType, jets: &mut impl Iterator<Item = Jet>) {
+    fn drop_rock(&self, rock: RockType, jets: &mut impl Iterator<Item=(usize, Jet)>) -> Vector2D {
         let mut pos = Vector2D::new(2, self.height + 3);
         loop {
             // Push left or right, if possible
-            let jet = jets.next().unwrap();
+            let (_, jet) = jets.next().unwrap();
             let next_pos = match jet {
                 Jet::Left => pos + Vector2D::new(-1, 0),
                 Jet::Right => pos + Vector2D::new(1, 0),
@@ -128,11 +128,10 @@ impl Tower {
             let next_pos = pos + Vector2D::new(0, -1);
             if self.overlaps(rock, next_pos) {
                 // Landed
-                break;
+                return pos;
             }
             pos = next_pos;
         }
-        self.place_rock(rock, pos)
     }
 }
 
@@ -140,17 +139,88 @@ impl Tower {
 pub fn part1(input: &[Jet]) -> i32 {
     let mut tower = Tower::default();
     let mut rocks = RockType::all().into_iter().cycle();
-    let mut jets = input.iter().cloned().cycle();
+    let mut jets = input.iter().cloned().enumerate().cycle();
     for _ in 0..2022 {
         let rock = rocks.next().unwrap();
-        tower.drop_rock(rock, &mut jets);
+        let pos = tower.drop_rock(rock, &mut jets);
+        tower.place_rock(rock, pos);
     }
     tower.height
 }
 
+fn find_deepest_drop(jets: &[Jet], cycles: usize) -> i32 {
+    let mut tower = Tower::default();
+    let mut rocks = RockType::all().into_iter().cycle();
+    let mut jets = jets.iter().cloned().enumerate().cycle();
+    let mut deepest = 0;
+    for _ in 0..cycles {
+        let rock = rocks.next().unwrap();
+        let pos = tower.drop_rock(rock, &mut jets);
+        tower.place_rock(rock, pos);
+        deepest = deepest.max(tower.height - pos.y());
+    }
+    deepest
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct State {
+    rock_index: usize,
+    jet_index: usize,
+    top: Vec<Vector2D>,
+}
+
+impl Tower {
+    fn capture(&self, rock_index: usize, jet_index: usize, depth: i32) -> State {
+        let mut top = Vec::new();
+        for y in (-depth)..=0 {
+            for x in 0..7 {
+                if self.blocks.contains(&Vector2D::new(x, self.height + y)) {
+                    top.push(Vector2D::new(x, y));
+                }
+            }
+        }
+        State {
+            rock_index,
+            jet_index,
+            top,
+        }
+    }
+}
+
 #[aoc(day17, part2)]
-pub fn part2(input: &[Jet]) -> i32 {
-    todo!()
+pub fn part2(input: &[Jet]) -> u64 {
+    let mut tower = Tower::default();
+    let mut rocks = RockType::all().into_iter().enumerate().cycle().peekable();
+    let mut jets = input.iter().cloned().enumerate().cycle().peekable();
+    let cache_depth = find_deepest_drop(input, 1000);
+    let target = 1_000_000_000_000u64;
+    let mut cache = HashMap::<State, (u64, i32)>::new();
+    // Find first cycle
+    let mut i = 0;
+    let (cycle_length, cycle_height) = loop {
+        let (rock_index, _) = *rocks.peek().unwrap();
+        let (jet_index, _) = *jets.peek().unwrap();
+        let state = tower.capture(rock_index, jet_index, cache_depth);
+        if let Some(&(previous_i, previous_height)) = cache.get(&state) {
+            break (i - previous_i, tower.height - previous_height);
+        }
+        cache.insert(state, (i, tower.height));
+        let (_, rock) = rocks.next().unwrap();
+        let pos = tower.drop_rock(rock, &mut jets);
+        tower.place_rock(rock, pos);
+        i += 1;
+    };
+    // Skip over repeated cycles
+    let cycle_count = (target - i) / cycle_length;
+    i += cycle_count * cycle_length;
+    // Finish remaining drops
+    while i < target {
+        let (_, rock) = rocks.next().unwrap();
+        let pos = tower.drop_rock(rock, &mut jets);
+        tower.place_rock(rock, pos);
+        i += 1;
+    }
+    tower.height as u64 + (cycle_count * cycle_height as u64)
 }
 
 #[cfg(test)]
@@ -170,6 +240,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let input = input_generator(&TEST_INPUT);
-        assert_eq!(part2(&input), 0);
+        assert_eq!(part2(&input), 1_514_285_714_288);
     }
 }
